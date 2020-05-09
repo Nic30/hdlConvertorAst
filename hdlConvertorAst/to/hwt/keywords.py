04518b842f94ec433e073fb15b8400818f45d643
Nic30 <nic30@seznam.cz>
2020-05-09 20:28:53 +0200

    HWT_KEYWORDS: rm duplicit items

--- hdlConvertorAst/to/hwt/keywords.py
+++ hdlConvertorAst/to/hwt/keywords.py
@@ -4,9 +4,7 @@ import keyword
 HWT_KEYWORDS = keyword.kwlist + [
     # imports
     'hwt', 'Unit', "Param", "Signal",
-    'power', 'Concat',
     "power", "Concat", "If", "Switch",
     "HArray", "HArrayVal", "Bits", "INT", "SLICE",
-    'Bits', "HEnum", "SliceVal",
-    "_",
+    "HEnum", "SliceVal", "_",
 ]
