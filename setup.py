de789435b133a939255e34355b4fcd8a211e7ce7
Nic30 <nic30@seznam.cz>
2020-05-11 17:01:27 +0200

    v0.1

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import os
 from setuptools import find_packages, setup
 import sys
 
+
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, "README.md")) as f:
     long_description = f.read()
