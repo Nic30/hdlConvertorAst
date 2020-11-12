9105f9322e06c7d1198f0e7d8df7b7446230c437
Nic30 <nic30@seznam.cz>
2020-11-12 08:51:07 +0100

    BasicHdlSimModelTranslateVerilogOperands: supprt for $display, $finish

--- hdlConvertorAst/translate/_verilog_to_basic_hdl_sim_model/verilog_operands_to_basic_hdl_sim_model.py
+++ hdlConvertorAst/translate/_verilog_to_basic_hdl_sim_model/verilog_operands_to_basic_hdl_sim_model.py
@@ -1,7 +1,7 @@
-from hdlConvertorAst.hdlAst._expr import HdlOpType, HdlOp, HdlValueId
+from hdlConvertorAst.hdlAst import HdlOpType, HdlOp, HdlValueId, HdlStmThrow
 from hdlConvertorAst.to.hdl_ast_modifier import HdlAstModifier
 from hdlConvertorAst.translate._verilog_to_basic_hdl_sim_model.utils import \
-    to_property_call
+    to_property_call, hdl_call
 
 
 class BasicHdlSimModelTranslateVerilogOperands(HdlAstModifier):
@@ -10,6 +10,7 @@ class BasicHdlSimModelTranslateVerilogOperands(HdlAstModifier):
         """
         :type o: HdlOp
         """
+        o = HdlAstModifier.visit_HdlOp(self, o)
         op = o.fn
         if op == HdlOpType.EQ:
             # HdlOpType.EQ: '%s._eq(%s)',
@@ -25,5 +26,14 @@ class BasicHdlSimModelTranslateVerilogOperands(HdlAstModifier):
         elif op == HdlOpType.TO:
             raise NotImplementedError(o)
         elif op == HdlOpType.CALL:
-            raise NotImplementedError("inline", o)
+            fn = o.ops[0]
+            if fn == HdlValueId("$display"):
+                o.ops = [HdlValueId("print"), ] + o.ops[1:]
+            elif fn == HdlValueId("$finish"):
+                t = HdlStmThrow()
+                t.val = hdl_call(HdlValueId("Exception"), [])
+                return t
+            else:
+                raise NotImplementedError("need to inline function", o)
+
         return o
