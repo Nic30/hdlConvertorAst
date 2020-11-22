7f0f724450243352aecdec00cbab8d9f7afc53f1
Nic30 <nic30@seznam.cz>
2020-11-23 00:27:12 +0100

    VerilogTypesToBasicHdlSimModel, VerilogTypesToHwt: improved width resol.

--- hdlConvertorAst/translate/_verilog_to_basic_hdl_sim_model/verilog_types_to_basic_hdl_sim_model.py
+++ hdlConvertorAst/translate/_verilog_to_basic_hdl_sim_model/verilog_types_to_basic_hdl_sim_model.py
@@ -1,8 +1,11 @@
 
-from hdlConvertorAst.hdlAst import HdlTypeBitsDef, HdlOpType, HdlOp
-from hdlConvertorAst.to.basic_hdl_sim_model.utils import BitsT
+from hdlConvertorAst.hdlAst import HdlTypeBitsDef, HdlOpType, HdlOp, HdlValueId,\
+    HdlTypeAuto, HdlValueInt
+from hdlConvertorAst.to.basic_hdl_sim_model.utils import BitsT,\
+    _verilog_slice_to_width
 from hdlConvertorAst.translate._verilog_to_basic_hdl_sim_model.utils import hdl_index
 from hdlConvertorAst.translate.verilog_resolve_types import VerilogResolveTypes
+from hdlConvertorAst.py_ver_compatibility import is_str
 
 
 class VerilogTypesToBasicHdlSimModel(VerilogResolveTypes):
@@ -17,14 +20,34 @@ class VerilogTypesToBasicHdlSimModel(VerilogResolveTypes):
             o0, o1 = t.ops
             return hdl_index(self._visit_type(o0), o1)
         elif isinstance(t, HdlTypeBitsDef):
-            return BitsT(t.msb, t.signed)
-        else:
-            raise NotImplementedError()
+            width = _verilog_slice_to_width(t.msb, t.lsb)
+            return BitsT(width, bool(t.signed))
+        elif isinstance(t, HdlValueId):
+            if t == HdlValueId("integer"):
+                return HdlValueId("INT")
+        raise NotImplementedError(t)
+
+    def visit_HdlIdDef(self, o):
+        """
+        :type o: HdlIdDef
+        """
+        if o.type is HdlTypeAuto:
+            if is_str(o.value):
+                o.type = HdlValueId("STR")
+                return o
+            elif isinstance(o.value, HdlValueInt):
+                v = o.value.val
+                if isinstance(v, int) and (v <= 1 and v >= 0):
+                    o.type =  HdlValueId("BIT")
+                else:
+                    o.type =  HdlValueId("INT")
+                return o
+                
+        return VerilogResolveTypes.visit_HdlIdDef(self, o)
 
     def visit_type(self, t):
         """
         :type t: iHdlExpr
-            def visit_type(self, t):
         """
         t = VerilogResolveTypes.visit_type(self, t)
         return self._visit_type(t)
--- hdlConvertorAst/translate/_verilog_to_hwt/verilog_types_to_hwt.py
+++ hdlConvertorAst/translate/_verilog_to_hwt/verilog_types_to_hwt.py
@@ -2,12 +2,13 @@ from typing import Union
 
 from hdlConvertorAst.hdlAst import HdlOp, HdlOpType, iHdlTypeDef, iHdlExpr, \
     HdlTypeBitsDef, HdlValueId
+from hdlConvertorAst.to.basic_hdl_sim_model.utils import _verilog_slice_to_width
 from hdlConvertorAst.to.hwt.utils import BitsT
 from hdlConvertorAst.translate._verilog_to_basic_hdl_sim_model.utils import hdl_index
-from hdlConvertorAst.translate.verilog_resolve_types import VerilogResolveTypes
+from hdlConvertorAst.translate._verilog_to_basic_hdl_sim_model.verilog_types_to_basic_hdl_sim_model import VerilogTypesToBasicHdlSimModel
 
 
-class VerilogTypesToHwt(VerilogResolveTypes):
+class VerilogTypesToHwt(VerilogTypesToBasicHdlSimModel):
 
     def _visit_type(self, t):
         """
@@ -17,15 +18,11 @@ class VerilogTypesToHwt(VerilogResolveTypes):
             o0, o1 = t.ops
             return hdl_index(self._visit_type(o0), o1)
         elif isinstance(t, HdlTypeBitsDef):
-            return BitsT(t.msb, t.signed)
+            width = _verilog_slice_to_width(t.msb, t.lsb)
+            if width == 1:
+                return HdlValueId("BIT")
+            return BitsT(width, t.signed)
         elif isinstance(t, HdlValueId):
             if t == HdlValueId("integer"):
                 return HdlValueId("INT")
         raise NotImplementedError(t)
-
-    def visit_type(self, t):
-        """
-        :type t: iHdlExpr
-        """
-        t = VerilogResolveTypes.visit_type(self, t)
-        return self._visit_type(t)
