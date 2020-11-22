e52c541eebdb5d74334aa825460a5301a7b1b3cb
Nic30 <nic30@seznam.cz>
2020-11-23 00:22:24 +0100

    ToHwtExpr: AND_UNARY, OR_UNARY, XOR_UNARY

--- hdlConvertorAst/to/hwt/expr.py
+++ hdlConvertorAst/to/hwt/expr.py
@@ -9,3 +9,26 @@ class ToHwtExpr(ToBasicHdlSimModelExpr):
     GENERIC_BIN_OPS.update({
         HdlOpType.DOWNTO: ":",
     })
+
+    def visit_HdlOp(self, o):
+        """
+        :type op: HdlOp
+        """
+        op = o.fn
+        if op in (HdlOpType.AND_UNARY, HdlOpType.OR_UNARY, HdlOpType.XOR_UNARY):
+            ops = o.ops
+            assert len(ops) == 1, ops
+            w = self.out.write
+            if HdlOpType.AND_UNARY:
+                w("And(*")
+            elif op is HdlOpType.OR_UNARY:
+                w("Or(*")
+            elif op is HdlOpType.XOR_UNARY:
+                w("Xor(*(")
+            else:
+                raise NotImplementedError()
+            self.visit_iHdlExpr(ops[0])
+            w("))")
+            return
+            
+        return ToBasicHdlSimModelExpr.visit_HdlOp(self, o)
