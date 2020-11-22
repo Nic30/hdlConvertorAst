1013ad717f6e0ecf8eaec1df99a61b89c500ff2c
Nic30 <nic30@seznam.cz>
2020-11-23 00:26:24 +0100

    refactoring: use ASSIGN_OPERATORS

--- hdlConvertorAst/translate/_verilog_to_hwt/signal_assignments_to_call_op.py
+++ hdlConvertorAst/translate/_verilog_to_hwt/signal_assignments_to_call_op.py
@@ -1,6 +1,7 @@
-from hdlConvertorAst.hdlAst import HdlOp, HdlOpType
+from hdlConvertorAst.hdlAst import HdlOpType
 from hdlConvertorAst.to.hdl_ast_modifier import HdlAstModifier
 from hdlConvertorAst.translate._verilog_to_basic_hdl_sim_model.utils import hdl_call
+from hdlConvertorAst.to.verilog.expr import ASSIGN_OPERATORS
 
 
 class SignalAssignmentsToCallOp(HdlAstModifier):
@@ -24,20 +25,7 @@ class SignalAssignmentsToCallOp(HdlAstModifier):
         if o.fn == HdlOpType.ASSIGN:
             dst, src = o.ops
             return hdl_call(dst, [src, ])
-        elif o.fn in [
-            HdlOpType.PLUS_ASSIGN,  # +=
-            HdlOpType.MINUS_ASSIGN,  # -=
-            HdlOpType.MUL_ASSIGN,  # *=
-            HdlOpType.DIV_ASSIGN,  # /=
-            HdlOpType.MOD_ASSIGN,  # %=
-            HdlOpType.AND_ASSIGN,  # &=
-            HdlOpType.OR_ASSIGN,  # |=
-            HdlOpType.XOR_ASSIGN,  # ^=
-            HdlOpType.SHIFT_LEFT_ASSIGN,  # <<=
-            HdlOpType.SHIFT_RIGHT_ASSIGN,  # >>=
-            HdlOpType.ARITH_SHIFT_LEFT_ASSIGN,  # <<<=
-            HdlOpType.ARITH_SHIFT_RIGHT_ASSIGN,  # >>>=
-           ]:
-            raise NotImplementedError() 
+        elif o.fn in ASSIGN_OPERATORS:
+            raise NotImplementedError(o) 
             
         return o
