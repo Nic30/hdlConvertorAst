f899cbe2ffef354ddb6bf3cad863894df37e9fdf
Nic30 <nic30@seznam.cz>
2021-04-14 23:36:52 +0200

    doc debug prints

--- doc/conf.py
+++ doc/conf.py
@@ -190,10 +190,14 @@ def setup(app):
     app.connect("autodoc-skip-member", skip)
 
 
-#root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-#sys.path.insert(0, root)
-#sys.path.insert(0, os.path.join(root, project))
-#os.environ["PYTHONPATH"] = ";".join(sys.path)
+# root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# sys.path.insert(0, root)
+# sys.path.insert(0, os.path.join(root, project))
+# os.environ["PYTHONPATH"] = ";".join(sys.path)
+print("CWD: ", os.getcwd())
+print("PYTHONPATH: ", os.environ.get("PYTHONPATH", ""))
+print("sys.path: ", sys.path)
+
 # update *.rst pages
 apidoc_main(["--module-first", "--full",
              "--output-dir", "../doc", "../hdlConvertorAst"])
