da39c140c96699fae20eb97ed7a7f2de1f01f547
Nic30 <nic30@seznam.cz>
2021-04-14 23:12:13 +0200

    doc: manually export PYTHONPATH

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ from sphinx.ext.apidoc import main as apidoc_main
 import sphinx_bootstrap_theme
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-
+os.environ["PYTHONPATH"] = ";".join(sys.path)
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
