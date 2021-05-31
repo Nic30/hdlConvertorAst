621c6802e9ace75be04fbb71d9b805c1a557ea6c
Nic30 <nic30@seznam.cz>
2021-05-31 20:22:32 +0200

    cleanup hwt keywords

--- hdlConvertorAst/to/hwt/keywords.py
+++ hdlConvertorAst/to/hwt/keywords.py
@@ -6,8 +6,8 @@ HWT_KEYWORDS = keyword.kwlist + [
     # imports
     'hwt', 'Unit', "Param", "Signal",
     "Concat", "If", "Switch",
-    "HArray", "HArrayVal", "Bits", "INT", "SLICE", "FLOAT64",
-    "HEnum", "SliceVal", "_", "rename_signal",
+    "HArray", "Bits", "INT", "SLICE", "STR", "BIT", "FLOAT64",
+    "HEnum", "_", "rename_signal",
 ]
 
 if sys.version_info[0] <= 2:
