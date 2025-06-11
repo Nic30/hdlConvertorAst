d41cd52b93df82b9cb3d6a8c6839af67edbbe8ca
Nic30 <nic30@seznam.cz>
2025-06-11 11:07:51 +0200

    feat(CodePosition): add file_name prop.
    https://github.com/Nic30/hdlConvertor/pull/191

--- hdlConvertorAst/hdlAst/utils.py
+++ hdlConvertorAst/hdlAst/utils.py
@@ -2,6 +2,7 @@
 
 class CodePosition(object):
     __slots__ = [
+        "file_name",
         "start_line",
         "start_column",
         "stop_line",
@@ -9,12 +10,13 @@ class CodePosition(object):
     ]
 
     def __init__(self):
+        self.file_name = None
         self.start_line = None
         self.start_column = None
         self.stop_line = None
         self.stop_column = None
 
     def __repr__(self):
-        return "<%s %r:%r to %r:%r>" % (self.__class__.__name__,
+        return "<%s %r:%r to %r:%r>" % (self.__class__.__name__, self.file_name,
                                         self.start_line, self.start_column,
                                         self.stop_line, self.stop_column)
